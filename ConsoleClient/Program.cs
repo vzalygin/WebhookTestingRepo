8f76423bf5da3b6683325370b0fe9eae2bf1e43d
Vyacheslav Zalygin <v.zalyginx@dodopizza.com>
2021-07-29 22:39:06 +0300

    Write some logic in ConsoleClient

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -1,13 +1,52 @@
 ﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR.Client;
 
 namespace ConsoleClient
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("hello world");
+            Console.WriteLine("ConsoleClient for DevОленька project.");
+            var connection = new HubConnectionBuilder()
+                .WithUrl("http://localhost:5000/hub")
+                .WithAutomaticReconnect()
+                .Build();
+            connection.On(
+                "Send",
+                () =>
+                {
+                    Console.WriteLine("New push!");
+                });
+            Console.WriteLine("Connection is configured");
+
+            var isConnected = false;
+            while (!isConnected)
+            {
+                try
+                {
+                    Console.WriteLine("Trying to connect to server...");
+                    await connection.StartAsync();
+                    Console.WriteLine("Successful!");
+                    isConnected = true;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(
+                        $"Err:\n" + 
+                        $"{e.Message}\n" +
+                        $"Try again in 5 sec..."
+                    );
+                    Thread.Sleep(5000);
+                }
+            }
+            
+            Console.WriteLine("Ready to work!");
+            
+            Console.ReadLine();
         }
     }
 }
